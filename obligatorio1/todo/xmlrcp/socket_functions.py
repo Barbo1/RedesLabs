ceb4375e27ce48b7a8ae138c950aab74bef955f5
mcastro <marcos.castro@fing.edu.uy>
2025-11-08 11:40:16 -0300

    correccion socket read ob1

--- obligatorio1/todo/xmlrcp/socket_functions.py
+++ obligatorio1/todo/xmlrcp/socket_functions.py
@@ -1,15 +1,48 @@
 import socket
+import re
 
+LF_CHAR = chr(10)
+CR_CHAR = chr(13)
+SP_CHAR = chr(32)
+FINISH_LINE = CR_CHAR + LF_CHAR
+HDR_END = FINISH_LINE + FINISH_LINE
 
 def read_socket(conn, buffer_size):
     http_req = b""
     status = True
+    content_length = 0
     try:
+        buf = bytearray()
+        header_bytes = b""
+        rest = b""
         while True:
             rec = conn.recv(buffer_size)
             if not rec:
                 break
-            http_req += rec
+
+            buf += rec
+
+            end = buf.find(HDR_END)
+            if end != -1:
+                header_bytes = bytes(buf[:end + len(HDR_END)])
+                rest = bytes(buf[end + len(HDR_END):])
+                break  # tenemos headers
+
+        regex = re.compile(rb"(?i)\bcontent-length:\s*(\d+)\b")
+        match = regex.search(header_bytes)
+        if match:
+            content_length = int(match.group(1))
+        body = bytearray(rest)
+
+        while len(body) < content_length:
+            rec = conn.recv(min(buffer_size, content_length - len(body)))
+            if not rec:
+                # conexión cerrada antes de tiempo
+                break
+            body += rec
+
+        http_req = header_bytes + bytes(body)
+
     except socket.timeout:
         pass
     except socket.error as ex:
